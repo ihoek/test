0bad727da59b99519f87a7c9749d8627b3e88b3a
ihoek <ihoek679@naver.com>
2025-01-20 20:27:24 +0900

    스토리지 input 1차 수정

--- 20250120/test1.js
+++ 20250120/test1.js
@@ -1,11 +1,18 @@
 //변수 선언
 let data_map = [];
 let cnt = 0;
+
 const id_input = document.getElementById("id_input");
 const name_input = document.getElementById("name_input");
 const age_input = document.getElementById("age_input");
 const career_input = document.getElementById("career_input");
 const nickname_input = document.getElementById("nickname_input");
+
+const error_id = document.getElementById("error_id");
+const error_name = document.getElementById("error_name");
+const error_age = document.getElementById("error_age");
+const error_career = document.getElementById("error_career");
+const error_nickname = document.getElementById("error_nickname");
 
 //중복 확인용 배열
 let id_arr = [];
@@ -29,13 +36,13 @@ window.onload = function () {
     for (let j in JSON.parse(ls)) {
       data_map.push(JSON.parse(ls)[j]);
       id_arr.push(data_map[j]._id);
+
       //row 생성
       const tr = document.createElement("tr");
       for (let i = 0; i < 4; i++) {
         const td = document.createElement("td");
         switch (i) {
           case 0:
-            //td.innerText = infoData.name;
             td.innerText = data_map[j].name;
             break;
 
@@ -49,6 +56,7 @@ window.onload = function () {
 
           case 3:
             td.innerText = data_map[j].nickname;
+            nickname_arr.push(data_map[j].nickname);
             break;
         }
 
@@ -77,57 +85,73 @@ save_btn.addEventListener("click", () => {
 
   //중복 값 확인
   let ck_id = id_input.value;
-  // let ck_nick = nickname_input.value;
+  let ck_nick = nickname_input.value;
 
   id_arr.push(infoData._id);
-  console.log(id_arr);
-  // for (let i in data_map) {
-  //   id_arr.push(data_map[i]._id);
-  //   console.log(id_arr);
-  // }
-  if (id_arr.includes(ck_id) === true) {
+  nickname_arr.push(infoData.nickname);
+
+  //조건에 맞게 입력하지 않았을 경우
+  if (
+    id_arr.slice(0, id_arr.length - 1).includes(ck_id) === true ||
+    nickname_arr.slice(0, nickname_arr.length - 1).includes(ck_nick) === true ||
+    career_input.value.length < 15 ||
+    nickname_input.value < 2
+  ) {
     //이미 입력된 값이라면
-    alert("1");
-  }
-
-  //local Storage set
-  window.localStorage.setItem("_data", JSON.stringify(data_map));
-  // for (let i in data_map) {
-  //   console.log(data_map[i]._id);
-  // }
-
-  //row 생성
-  const tr = document.createElement("tr");
-  for (let i = 0; i < 4; i++) {
-    const td = document.createElement("td");
-    switch (i) {
-      case 0:
-        td.innerText = infoData.name;
-        break;
-
-      case 1:
-        td.innerText = infoData.age;
-        break;
-
-      case 2:
-        td.innerText = infoData.career;
-        break;
-
-      case 3:
-        td.innerText = infoData.nickname;
-        break;
+    if (id_arr.slice(0, id_arr.length - 1).includes(ck_id) === true) {
+      error_id.innerText = "아이디를 중복 입력하셨습니다.";
+    } else if (
+      nickname_arr.slice(0, nickname_arr.length - 1).includes(ck_nick) === true
+    ) {
+      error_nickname.innerText = "별명을 중복 입력하셨습니다.";
+    } else if (career_input.value.length < 15) {
+      error_career.innerText = "15자 이상으로 입력하시오.";
+    } else if (nickname_input.value < 2) {
+      error_career.innerText = "2자 이상으로 입력하시오.";
     }
-
-    tr.appendChild(td);
-    table.appendChild(tr);
 
     id_input.value = null;
     name_input.value = null;
     age_input.value = null;
     career_input.value = null;
     nickname_input.value = null;
+  } else {
+    //row 생성
+    const tr = document.createElement("tr");
+    for (let i = 0; i < 4; i++) {
+      const td = document.createElement("td");
+      switch (i) {
+        case 0:
+          td.innerText = infoData.name;
+          break;
+
+        case 1:
+          td.innerText = infoData.age;
+          break;
+
+        case 2:
+          td.innerText = infoData.career;
+          break;
+
+        case 3:
+          td.innerText = infoData.nickname;
+          break;
+      }
+
+      tr.appendChild(td);
+      table.appendChild(tr);
+
+      id_input.value = null;
+      name_input.value = null;
+      age_input.value = null;
+      career_input.value = null;
+      nickname_input.value = null;
+    }
+    cnt++;
+
+    //local Storage set
+    window.localStorage.setItem("_data", JSON.stringify(data_map));
   }
-  cnt++;
 });
 
 //table
