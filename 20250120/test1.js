7ef2c70495150d26c48fecd0f8a684d986b8c532
ihoek <ihoek679@naver.com>
2025-01-20 23:09:31 +0900

    20250120 js 수정

--- 20250120/test1.js
+++ 20250120/test1.js
@@ -32,7 +32,7 @@ window.onload = function () {
   } else {
     console.log("로드 시 값이 존재 함");
     //console.log(typeof ls); //스트링
-
+    console.log("로드 시 값 (JSON.parse(ls)): ", JSON.parse(ls));
     for (let j in JSON.parse(ls)) {
       data_map.push(JSON.parse(ls)[j]);
       id_arr.push(data_map[j]._id);
@@ -41,28 +41,21 @@ window.onload = function () {
       const tr = document.createElement("tr");
       for (let i = 0; i < 4; i++) {
         const td = document.createElement("td");
-        switch (i) {
-          case 0:
-            td.innerText = data_map[j].name;
-            break;
-
-          case 1:
-            td.innerText = data_map[j].age;
-            break;
-
-          case 2:
-            td.innerText = data_map[j].career;
-            break;
-
-          case 3:
-            td.innerText = data_map[j].nickname;
-            nickname_arr.push(data_map[j].nickname);
-            break;
+        if (i === 0) {
+          td.innerText = data_map[j].name;
+        } else if (i === 1) {
+          td.innerText = data_map[j].age;
+        } else if (i === 2) {
+          td.innerText = data_map[j].career;
+        } else if (i === 3) {
+          td.innerText = data_map[j].nickname;
+          nickname_arr.push(data_map[j].nickname);
         }
 
         tr.appendChild(td);
         table.appendChild(tr);
       }
+      console.log("로드 시 값 (data_map): ", data_map);
     }
   }
 };
@@ -91,6 +84,7 @@ save_btn.addEventListener("click", () => {
   nickname_arr.push(infoData.nickname);
 
   //조건에 맞게 입력하지 않았을 경우
+
   if (
     id_arr.slice(0, id_arr.length - 1).includes(ck_id) === true ||
     nickname_arr.slice(0, nickname_arr.length - 1).includes(ck_nick) === true ||
@@ -109,44 +103,39 @@ save_btn.addEventListener("click", () => {
     } else if (nickname_input.value < 2) {
       error_career.innerText = "2자 이상으로 입력하시오.";
     }
-
-    id_input.value = null;
-    name_input.value = null;
-    age_input.value = null;
-    career_input.value = null;
-    nickname_input.value = null;
+    window.localStorage.removeItem(data_map);
+    id_input.value = "";
+    name_input.value = "";
+    age_input.value = "";
+    career_input.value = "";
+    nickname_input.value = "";
   } else {
     //row 생성
     const tr = document.createElement("tr");
-    for (let i = 0; i < 4; i++) {
-      const td = document.createElement("td");
-      switch (i) {
-        case 0:
-          td.innerText = infoData.name;
-          break;
 
-        case 1:
-          td.innerText = infoData.age;
-          break;
+    const td_name = document.createElement("td");
+    const td_age = document.createElement("td");
+    const td_career = document.createElement("td");
+    const td_nickname = document.createElement("td");
 
-        case 2:
-          td.innerText = infoData.career;
-          break;
+    td_name.innerText = infoData.name;
+    td_age.innerText = infoData.age;
+    td_career.innerText = infoData.career;
+    td_nickname.innerText = infoData.nickname;
 
-        case 3:
-          td.innerText = infoData.nickname;
-          break;
-      }
+    tr.appendChild(td_name);
+    tr.appendChild(td_age);
+    tr.appendChild(td_career);
+    tr.appendChild(td_nickname);
 
-      tr.appendChild(td);
-      table.appendChild(tr);
+    table.appendChild(tr);
 
-      id_input.value = null;
-      name_input.value = null;
-      age_input.value = null;
-      career_input.value = null;
-      nickname_input.value = null;
-    }
+    id_input.value = "";
+    name_input.value = "";
+    age_input.value = "";
+    career_input.value = "";
+    nickname_input.value = "";
+
     cnt++;
 
     //local Storage set
